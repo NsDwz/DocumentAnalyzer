6a10f9932f68ab11813fbeac8db65c1eaf95ca84
rkostyuk <rkostyuk@hts-itayly.com>
2023-08-10 16:06:56 +0200

    first commit

---
+++ AIBasedServises/DocumentRecognizerService.cs
@@ -0,0 +1,30 @@
+﻿using Azure;
+using Azure.AI.FormRecognizer.DocumentAnalysis;
+
+namespace AIBasedServises;
+
+public class DocumentRecognizerService
+{
+    // private string _endpoint;
+    // private string _apiKey;
+    private DocumentAnalysisClient _client;
+    
+    public DocumentRecognizerService(string endpoint, string apiKey)
+    {
+        var credential = new AzureKeyCredential(apiKey);
+        _client = new DocumentAnalysisClient(new Uri(endpoint), credential);
+    }
+
+    public void ExtractDocumentInformation(Stream fs)
+    {
+        AnalyzeDocumentOperation operation = _client.AnalyzeDocument(WaitUntil.Completed, "prebuilt-layout", fs);
+        AnalyzeResult result = operation.Value;
+        
+        foreach (DocumentPage page in result.Pages)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+
+    
+}
---
+++ DocumentEstraction/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AIBasedServises;
+
+Console.WriteLine("Hello, World!");
+var analizer = new DocumentRecognizerService("https://firststep.cognitiveservices.azure.com/", "0b6f1a4ed5a943a1a31f3b2a10c85031");
+string path = @"./documento-unico-de-identidade.jpg";
+
+if (File.Exists(path))
+{
+    File.Delete(path);
+}
+
+using (FileStream fs = File.Create(path))
+{
+    
+    analizer.ExtractDocumentInformation(fs);
+}
